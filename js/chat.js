89d0ca0ea745f2c22ad22880e3f0fa0e6985a455
im-rohit26 <ghoshrohit977@gmail.com>
2024-09-24 17:55:59 +0530

    changes

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,7 @@
 $(function() {
   var INDEX = 0;
 
+  // Function to send message to the backend (OpenAI API)
   async function sendMessageToBackend(message) {
     try {
       const response = await fetch('/api/openai', {
@@ -23,6 +24,7 @@ $(function() {
     }
   }
 
+  // Click event for submitting user messages
   $("#chat-submit").click(function(e) {
     e.preventDefault();
     var msg = $("#chat-input").val();
@@ -33,36 +35,39 @@ $(function() {
     sendMessageToBackend(msg);
   });
 
+  // Function to generate message in chat
   function generate_message(msg, type) {
     INDEX++;
-    var avatar = (type === 'self') ? "user.png" : "chatbot.png";
+    // Avatar logic removed for bot as per new design
     var str = "";
     str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg " + type + "\">";
-    str += "          <span class=\"msg-avatar\">";
-    str += "            <img src=\"images/" + avatar + "\">";
-    str += "          <\/span>";
+    if (type === 'user') {
+      str += "          <span class=\"msg-avatar\">";
+      str += "            <img src=\"images/user.png\">"; // User avatar remains
+      str += "          <\/span>";
+    }
     str += "          <div class=\"cm-msg-text\">";
     str += msg;
     str += "          <\/div>";
     str += "        <\/div>";
+    
     $(".chat-logs").append(str);
     $("#cm-msg-" + INDEX).hide().fadeIn(300);
+
     if (type == 'self') {
       $("#chat-input").val('');
     }
     $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
   }
 
+  // Function to generate buttons (used for possible bot choices)
   function generate_button_message(msg, buttons) {
     INDEX++;
     var btn_obj = buttons.map(function(button) {
       return "              <li class=\"button\"><a href=\"javascript:;\" class=\"btn btn-primary chat-btn\" chat-value=\"" + button.value + "\">" + button.name + "<\/a><\/li>";
     }).join('');
     var str = "";
-    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg user\">";
-    str += "          <span class=\"msg-avatar\">";
-    str += "            <img src=\"images/chatbot.png\">";
-    str += "          <\/span>";
+    str += "<div id='cm-msg-" + INDEX + "' class=\"chat-msg bot\">";
     str += "          <div class=\"cm-msg-text\">";
     str += msg;
     str += "          <\/div>";
@@ -72,12 +77,14 @@ $(function() {
     str += "            <\/ul>";
     str += "          <\/div>";
     str += "        <\/div>";
+
     $(".chat-logs").append(str);
     $("#cm-msg-" + INDEX).hide().fadeIn(300);
     $(".chat-logs").stop().animate({ scrollTop: $(".chat-logs")[0].scrollHeight }, 1000);
     $("#chat-input").attr("disabled", true);
   }
 
+  // Click event for chat buttons (e.g., bot response options)
   $(document).delegate(".chat-btn", "click", function() {
     var value = $(this).attr("chat-value");
     var name = $(this).html();
@@ -85,6 +92,7 @@ $(function() {
     generate_message(name, 'self');
   });
 
+  // Function to end a session (reset chat)
   function endSession() {
     $(".chat-logs").empty();
     $("#chat-input").val('');
@@ -92,25 +100,30 @@ $(function() {
     INDEX = 0;
   }
 
+  // Open the chat box
   $("#chat-circle").click(function() {
     $("#chat-circle").toggle('scale');
     $(".chat-box").toggle('scale');
   });
 
+  // Close the chat box and reset session
   $(".chat-box-toggle").click(function() {
     $("#chat-circle").toggle('scale');
     $(".chat-box").toggle('scale');
-    endSession();
+    endSession();  // End session on close
   });
 
+  // End session when close button clicked
   $("#close-session").click(function() {
     endSession();
   });
 
+  // Start a new session when chatbot is reopened
   $("#chat-circle").click(function() {
     startNewSession();
   });
 
+  // Re-enable input for new session
   function startNewSession() {
     $("#chat-input").attr("disabled", false);
   }
